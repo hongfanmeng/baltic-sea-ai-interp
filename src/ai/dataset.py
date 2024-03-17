516c2122d38429032188176a3afef8db52c6095c
Steven Hong <hongfanmeng@gmail.com>
2024-03-17 20:35:33 +0800

    feat: add dataset for VAE training

--- src/ai/dataset.py
+++ src/ai/dataset.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pandas as pd
 from torch.utils.data import Dataset
+from tqdm.contrib.concurrent import process_map
 
 base_dir = Path(__file__).resolve().parent.parent.parent
 
@@ -13,12 +14,19 @@ def read_depth():
     return df
 
 
+def read_mean_std():
+    with open(base_dir / "data/train_mean_std.pkl", "rb") as f:
+        mean, std = pickle.load(f)
+    return mean, std
+
+
 class BalticSeaDataset(Dataset):
-    def __init__(self, data_path=base_dir / "data/train_data.pkl"):
-        with open(data_path, "rb") as f:
-            self.mean, self.std, self.data_list = pickle.load(f)
-            self.data_list = list(self.data_list.groupby(["year", "mon", "lat", "lon"]))
+    def __init__(self):
+        self.data_list = pd.read_parquet(base_dir / "data/train_data.parquet")
+        self.data_list = list(self.data_list.groupby(["year", "mon", "lat", "lon"]))
+
         self.df_dep = read_depth()
+        self.mean, self.std = read_mean_std()
 
     def __len__(self):
         return len(self.data_list)
@@ -30,4 +38,44 @@ class BalticSeaDataset(Dataset):
 
         meta = pd.Series({"year": year, "mon": mon, "lat": lat, "lon": lon, "max_dep": max_dep})
 
+        return meta, data.reset_index(drop=True)
+
+
+class VaeDataset(Dataset):
+
+    def __init__(self):
+        print("reading data...")
+        self.df_dep = read_depth()
+        self.mean, self.std = read_mean_std()
+        vae_train_data = pd.read_pickle("data/vae_train_data.pkl")
+
+        print("filtering data for training...")
+        # use data with max_dep >= 50 to train
+        data_list = [(meta, data) for (meta, data) in vae_train_data if meta["max_dep"] >= 50]
+        fill_rate = process_map(self.get_fill_rate, data_list, max_workers=16, chunksize=1000)
+
+        # use data with fill rate >= 1 to train
+        self.data_list = [data for data, rate in zip(data_list, fill_rate) if rate >= 1]
+
+    @staticmethod
+    def get_fill_rate(data: tuple[pd.Series, pd.DataFrame]):
+        meta, data = data
+        total = sum(data["dep"] <= meta["max_dep"])
+        return len(data[data["dep"] <= meta["max_dep"]].dropna()) / total if total > 0 else 0
+
+    def preprocess(self, group: tuple[tuple, pd.DataFrame]):
+        meta, data = group
+        year, mon, lat, lon = meta
+        max_dep = self.df_dep.loc[(lon, lat), "dep"] if (lon, lat) in self.df_dep.index else 0
+        meta = pd.Series({"year": year, "mon": mon, "lat": lat, "lon": lon, "max_dep": max_dep})
+
+        return meta, data
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, idx) -> tuple[pd.Series, pd.DataFrame]:
+        meta, data = self.data_list[idx]
+        data = data.copy()
+        data[["oxy", "tmp", "sal"]] = (data[["oxy", "tmp", "sal"]] - self.mean) / self.std
         return meta, data.reset_index(drop=True)
--- src/ai/prepare_train_data.py
+++ src/ai/prepare_train_data.py
@@ -1,19 +1,23 @@
-import pickle
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
 
 base_dir = Path(__file__).resolve().parent.parent.parent
 
 # depth grid
-# 0 - 100m, every 5m, 100 - 250 m, every 10m
-dep_lb = np.concatenate((np.arange(0, 100, 5), np.arange(100, 250, 10)))
-dep_ub = np.concatenate((np.arange(0, 100, 5) + 5, np.arange(100, 250, 10) + 10))
+# 0 - 100m, every 5m, 100 - 200 m, every 10m
+dep_lb = np.concatenate((np.arange(0, 100, 5), np.arange(100, 200, 10)))
+dep_ub = np.concatenate((np.arange(0, 100, 5) + 5, np.arange(100, 200, 10) + 10))
+dep_mid = (dep_lb + dep_ub) / 2
+
+df_depth = pd.read_parquet(base_dir / "data/baltic_sea_depth.parquet")
+df_depth = df_depth.set_index(["lon", "lat"])
 
 
-if __name__ == "__main__":
+def prepare_train_data():
     df = pd.read_parquet(base_dir / "data/nest_baltic_sea.parquet")
 
     # filter qc flag
@@ -39,17 +43,47 @@ if __name__ == "__main__":
     std = df.std(numeric_only=True)[["oxy", "tmp", "sal"]]
 
     # grid dep
+    df = df.reset_index(drop=True)
+    df = df.drop(df[df["dep"] < dep_lb[0]].index).reset_index(drop=True)
+    df = df.drop(df[df["dep"] >= dep_ub[-1]].index).reset_index(drop=True)
     for lb, ub in tqdm(zip(dep_lb, dep_ub)):
         mid = (lb + ub) / 2
         df.loc[(lb <= df["dep"]) & (df["dep"] < ub), "dep"] = mid
-
-    df.drop(df[df["dep"] > dep_ub[-1]].index, inplace=True)
     df.reset_index(drop=True, inplace=True)
 
-    groups = df.groupby(["year", "mon", "lat", "lon", "dep"]).mean()
-    groups = groups.reset_index()
+    df = df.groupby(["year", "mon", "lat", "lon", "dep"]).mean().reset_index()
 
-    # save mean, std, data
-    output_data = (mean, std, groups)
-    with open(base_dir / "data/train_data.pkl", "wb") as f:
-        pickle.dump(output_data, f)
+    return mean, std, df
+
+
+def convert_vae_data(data_entry: tuple[tuple, pd.DataFrame]):
+    meta, data = data_entry
+    year, mon, lat, lon = meta
+    max_dep = df_depth.loc[(lon, lat), "dep"] if (lon, lat) in df_depth.index else 0
+
+    data_vae = pd.DataFrame(columns=["dep", "oxy", "tmp", "sal"])
+    data_vae["dep"] = dep_mid
+
+    data_vae = data_vae.set_index("dep")
+    data_vae.loc[data["dep"], ["oxy", "tmp", "sal"]] = data[["oxy", "tmp", "sal"]].values
+    data_vae.reset_index(inplace=True)
+
+    meta = pd.Series({"year": year, "mon": mon, "lat": lat, "lon": lon, "max_dep": max_dep})
+
+    return meta, data_vae
+
+
+def prepare_vae_data(train_data: pd.DataFrame):
+    data_list = train_data.groupby(["year", "mon", "lat", "lon"])
+    vae_train_data = process_map(convert_vae_data, data_list, max_workers=12, chunksize=10000)
+    return vae_train_data
+
+
+if __name__ == "__main__":
+    mean, std, train_data = prepare_train_data()
+    vae_train_data = prepare_vae_data(train_data)
+
+    pd.to_pickle(mean, base_dir / "data/mean.pkl")
+    train_data.to_parquet(base_dir / "data/train_data.parquet")
+
+    pd.to_pickle(vae_train_data, base_dir / "data/vae_train_data.parquet")
