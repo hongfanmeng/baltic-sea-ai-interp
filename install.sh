1074a8ec27b723f11622d13ba8e9fa13dd83c319
Steven Hong <hongfanmeng@gmail.com>
2024-06-30 15:59:09 +0800

    chore: change vae model and install script

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ conda create -n baltic python=3.11
 conda activate baltic
 
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
-pip install pyyaml numpy pandas scikit-learn tqdm httpx lightning matplotlib rioxarray pyarrow
+pip install pyyaml numpy pandas scikit-learn tqdm httpx lightning matplotlib rioxarray pyarrow tensorboard
